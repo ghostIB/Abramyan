eb63b2ea0a043f6d99d74dea4a49cd17b156b765
GhostIB <butsenko.ivansh@gmail.com>
2019-10-09 21:25:42 +0300

    add dll files

---
+++ DLL/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public static class SomeFunctions
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public static int ShowArray(Array arr)
+        {
+            foreach (var x in arr) Console.Write(x+" ");
+            Console.WriteLine();
+            return 0;
+        }
+        public static bool IsPairing(int a)
+        {
+            return a % 2 == 0;
+        }
+        public static int Count(Array arr,int a)
+        {
+            int k = 0;
+            foreach (int x in arr)
+            {
+                if (x == a)
+                {
+                    k++;
+                }
+            }
+            return k;
+        }
+        public static bool IsPrime(int a)
+        {
+            for (int i = 0; i <= a / 2; i++)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static bool IsPall(int a)
+        {
+            bool flag = true;
+            string a1 = a.ToString();
+            int len = a.ToString().Length;
+            for (int i = 0; i < len / 2; i++)
+            {
+                if (a1.Substring(i, 0) == a1.Substring(len - 1, 0))
+                {
+                    continue;
+                }
+                flag = false;
+                break;
+            }
+            return flag;
+        }
+    }
+}
