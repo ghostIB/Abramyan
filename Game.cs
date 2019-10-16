5194d3a97107ef3ac8be7d2875da1ebe9b0af591
GhostIB <butsenko.ivansh@gmail.com>
2019-10-16 22:19:25 +0300

    finish Game.cs

--- Game.cs
+++ Game.cs
@@ -12,42 +12,97 @@ namespace ConsoleApp24
         static string wall = "#";
         static string finish = "F";
         static string cell = " ";
-        static string[] symbols = { wall, cell, person, finish };
+        static string[] symbols = { wall, cell,cell,cell};
         static int height = 10;
         static int width = 12;
+        static string[,] zone=getZone(height,width);
         static void Main(string[] args)
         {
-            Generate(height, width);
-            Console.ReadKey();
+            Init(height,width);
         }
-        static void Generate(int h, int w)
+        static string[,] getZone(int h, int w)
         {
             var rand = new Random();
-            string[,] zone = new string[h, w];
-            int count_F = 0;
-            int count_Player = 0;
+            string[,] game_zone = new string[h, w];
             for (int i = 0; i < h; i++)
             {
                 for (int j = 0; j < w; j++)
                 { 
                     int Index = rand.Next(symbols.Length);
-                    if (Index==2)
-                    {
-                        count_Player += 1;
-                    }
-                    else if (Index == 3)
-                    {
-                        count_F += 1;
-                    }
-                    while ((count_F>1 || count_Player > 1) && (Index==2 || Index==3))
-                    {
-                        Index = rand.Next(symbols.Length);
-                    }
-                    zone[i, j] = symbols[Index];
-                    Console.Write("{0}\t", zone[i, j]);
+                    game_zone[i, j] = symbols[Index];
+                }
+            }
+            int charX=rand.Next(w);
+            int charY=rand.Next(h);
+            game_zone[charY,charX]=person;
+            game_zone[rand.Next(h),rand.Next(w)]=finish;
+            return game_zone;
+        }
+        static void Generate(int heig,int widt){
+          Console.Clear();
+          for (int i = 0; i < heig; i++)
+            {
+                for (int j = 0; j < widt; j++)
+                { 
+                    Console.Write("{0}\t",zone[i,j]);
                 }
                 Console.WriteLine();
             }
+        }
+        static void Init(int hei,int wid){
+          while (true){
+            Generate(hei,wid);
+            int[] k=Input();
+            if (IsFinish(k[0],k[1])){
+              break;
+            }
+            Move(k[0],k[1]);
+          }
+          Console.Clear();
+          Console.WriteLine("”ра вы дошли!!");
+        }
+        static int[] Input(){
+          var key=Console.ReadKey().Key;
+          switch(key){
+            case ConsoleKey.W:
+              return new int[] {-1,0};
+            case ConsoleKey.S:
+              return new int[] {1,0};
+            case ConsoleKey.A:
+              return new int[] {0,-1};
+            case ConsoleKey.D:
+              return new int[] {0,1};
+            default:
+              return new int[] {0,0};
+          }
+        }
+        static int[] getCharacterCoor(){
+          for (int i = 0; i < zone.GetLength(0); i++)
+            {
+                for (int j = 0; j < zone.GetLength(1); j++)
+                { 
+                    if (zone[i,j]=="@"){
+                      return new int[] {i,j};
+                    }
+                }
+            }
+            return new int[] {0,0};
+        }
+        static void Move(int Y,int X){
+          int PersY=getCharacterCoor()[0];
+          int PersX=getCharacterCoor()[1];
+          if (PersX+X>=0&&PersY+Y>=0 && !IsWall(PersY+Y,PersX+X)){
+            zone[PersY,PersX]=" ";
+            zone[PersY+Y,PersX+X]="@";
+          }
+        }
+        static bool IsWall(int coordinateY,int coordinateX){
+          return zone[coordinateY,coordinateX]=="#";
+        }
+        static bool IsFinish(int dy,int dx){
+          int PersY=getCharacterCoor()[0];
+          int PersX=getCharacterCoor()[1];
+          return zone[PersY+dy,PersX+dx]=="F";
         }
     }
 }
