62794f1453bb935ee1f28bb66f58a3784a58a54b
ghostIB <55488287+ghostIB@users.noreply.github.com>
2020-01-22 15:39:48 +0200

    add Tree class to BinaryTree.cs

--- BinaryTree.cs
+++ BinaryTree.cs
@@ -1,67 +1,107 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class MainClass {
-  public static void Main (string[] args) {
-    Node a=new Node(5);
-    a.AddValue(8);
-    a.AddValue(3);
-    a.AddValue(12);
-    Console.WriteLine(a.SearchNode(new Node(12)));
-  }
-  class Node{
-    public int value{get;}
-    public Node left;
-    public Node right;
-    private bool resultRight,resultLeft;
-    public Node(int inputValue){
-      this.value=inputValue;
-    }
-    public void AddValue(int nodeValue){
-      if (nodeValue<=value){
-        if (left==null){
-          left=new Node(nodeValue);
+namespace ConsoleApp56
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Tree a = new Tree(new Node(5));
+            Console.WriteLine(a.rootNode);
+            Console.ReadKey();
         }
-        else{
-          left.AddValue(nodeValue);
+        class Node
+        {
+
+            public int value { get; }
+            public Node left;
+            public Node right;
+            private int count = 1;
+            private bool resultRight, resultLeft;
+            public Node(int inputValue)
+            {
+                this.value = inputValue;
+            }
+            public void AddValue(int nodeValue)
+            {
+                count++;
+                if (nodeValue <= value)
+                {
+                    if (left == null)
+                    {
+                        left = new Node(nodeValue);
+                    }
+                    else
+                    {
+                        left.AddValue(nodeValue);
+                    }
+                }
+                else
+                {
+                    if (right == null)
+                    {
+                        right = new Node(nodeValue);
+                    }
+                    else
+                    {
+                        right.AddValue(nodeValue);
+                    }
+                }
+            }
+            public bool SearchNode(Node searchedNode)
+            {
+                if (searchedNode.value == value) return true;
+                else
+                {
+                    resultLeft = left == null ? false : left.SearchNode(searchedNode);
+                    resultRight = right == null ? false : right.SearchNode(searchedNode);
+                }
+                return resultLeft || resultRight;
+            }
+            public void GetSortedList()
+            {
+                if (left != null) left.GetSortedList();
+                Console.Write(value.ToString() + " ");
+                if (right != null) right.GetSortedList();
+            }
+            public bool CheckNodes(int checkedValue)
+            {
+                return right.value == checkedValue || left.value == checkedValue;
+            }
+            public int CountNodes() { return count; }
+            public override string ToString()
+            {
+                return $"{value}";
+            }
         }
-      }
-      else{
-        if (right==null){
-          right=new Node(nodeValue);
+        class Tree
+        {
+            public Node rootNode { get; }
+            public Tree(Node inputRootNode)
+            {
+                rootNode = inputRootNode;
+            }
+            public Node left;
+            public Node right;
+            private int countNode = 1;
+            private bool resultRight, resultLeft;
+            public void AddNode(Node inputNode)
+            {
+                if (inputNode.left.value != null) this.AddNode(inputNode.left);
+                this.rootNode.AddValue(inputNode);
+                if (inputNode.right.value != null) this.AddValue(inputNode.right);
+            }
         }
-        else{
-          right.AddValue(nodeValue);
+        public static void WriteLineArray(int[] array)
+        {   foreach (int i in array)
+            {
+                Console.Write(i.ToString() + " ");
+            }
+            Console.Write("\n");
         }
-      }
     }
-    public bool SearchNode(Node searchedNode){
-      if (searchedNode.value==value) return true;
-      else{
-        resultLeft = left==null?false:left.SearchNode(searchedNode);
-        resultRight = right==null?false:right.SearchNode(searchedNode);
-      }
-      return resultLeft||resultRight;
-    }
-    public bool CheckNodes(int checkedValue){
-      return right.value==checkedValue||left.value==checkedValue;
-    }
-    public int[] GetSortedNode(){
-      return new int[] {left.value,value,right.value};
-    }
-    public override string ToString(){
-      return $"{value}";
-    }
-  }
-  class Tree{
-    protected Node rootNode;
-    public Tree(int inputRootValue){
-      rootNode=new Node(inputRootValue);
-    }
-  }
-  public static void WriteLineArray(int[] array){
-    foreach (int i in array){
-      Console.Write(i.ToString()+" ");
-    }
-    Console.Write("\n");
-  }
 }
